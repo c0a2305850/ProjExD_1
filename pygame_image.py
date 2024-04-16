d92609f9472264fae892b8adccd629d18927607f
K.tetta <c0a2305850@edu.teu.ac.jp>
2024-04-16 16:31:53 +0900

    練習6

--- pygame_image.py
+++ pygame_image.py
@@ -13,16 +13,15 @@ def main():
     tmr = 0
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
-    kk_img = pg.transform.rotozoom(kk_img, 10, 1.0)
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [300, 200])
+        x = tmr%800#練習6
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(kk_img, [300, 200])#練習4
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(200)#練習5
 
 
 if __name__ == "__main__":
