5c24834e2dd8456037ac39a0837ae153321fa5a8
K.tetta <c0a2305850@edu.teu.ac.jp>
2024-04-16 18:14:51 +0900

    演習１

--- pygame_image.py
+++ pygame_image.py
@@ -16,19 +16,23 @@ def main():
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
     bg_img2 = pg.transform.flip(bg_img, True, False)#練習7,1
+    
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         key_lst = pg.key.get_pressed()
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip([0, -1])
+            kk_rct.move_ip((0, -1))
         if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip([0, 1])
+            kk_rct.move_ip((0, 1))
         if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip([-1, 0])
+            kk_rct.move_ip((-1, 0))
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip([1, 0])#練習8
-        x = tmr%3200#練習6
+            kk_rct.move_ip((2, 0))
+        else:
+            kk_rct.move_ip((-1, 0))
+
+        x = -tmr%3200#練習6
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_img2, [-x+1600, 0])
         screen.blit(bg_img, [-x+3200, 0])
@@ -36,7 +40,7 @@ def main():
         screen.blit(kk_img, kk_rct)#練習4
         
         pg.display.update()
-        tmr += 1        
+        tmr += 1   
         clock.tick(200)#練習5
 
 
